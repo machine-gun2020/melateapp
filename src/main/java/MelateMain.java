eec6555258de11ac42d23a986dd7c247e0301cb4
machine-gun2020 <76742771+machine-gun2020@users.noreply.github.com>
2026-02-17 15:14:29 -0600

    actualizacion de resultdos y probabilidad!

--- src/main/java/MelateMain.java
+++ src/main/java/MelateMain.java
@@ -11,6 +11,7 @@ public class MelateMain {
     public static void main(String... args) {
         System.out.println("🚀 Iniciando Microservicio de Sorteos");
         System.out.println("Pronostico");
+        System.out.println("Final");
         Quarkus.run(args);
     }
 }
